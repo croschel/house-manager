c096fb87fc3f9eeff146e534d3b982e578c87141
croschel <croschel000@gmail.com>
2024-12-10 18:47:48 -0300

    chore: clean up

--- web/src/components/generic/date-picker.tsx
+++ web/src/components/generic/date-picker.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { CalendarIcon } from '@radix-ui/react-icons';
-import { format, formatDate, formatISO } from 'date-fns';
+import { format, formatISO } from 'date-fns';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
