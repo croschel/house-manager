582ac021a6c9a55b7f9cd4ee6195f572b02d29d3
croschel <croschel000@gmail.com>
2025-07-28 00:14:57 -0300

    feat: Improve loading on market

--- web/src/pages/market-control/shopping/index.tsx
+++ web/src/pages/market-control/shopping/index.tsx
@@ -3,10 +3,12 @@ import { Conditional } from '@/components/generic/conditional';
 import { MainContainer } from '@/components/generic/main-container';
 import { MainFilterPage } from '@/components/generic/main-filter-page';
 import SidebarComponent from '@/components/generic/sidebar-component';
+import { LoadingSpinner } from '@/components/generic/spinner';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { PageType, StatusList } from '@/models/enums';
+import { ActionStatus, PageType, StatusList } from '@/models/enums';
 import { MarketList, Product } from '@/models/interfaces';
 import { useAppDispatch, useAppSelector } from '@/reducers';
+import { selectUpdateMarketListLoading } from '@/reducers/loading/selectors';
 import { updateMarketList } from '@/reducers/market/actions';
 import { selectMarketListSelected } from '@/reducers/market/selectors';
 import { useEffect, useState } from 'react';
@@ -17,6 +19,9 @@ export const Shopping = () => {
   const params = useParams();
   const navigate = useNavigate();
   const selectedMarketList = useAppSelector(selectMarketListSelected);
+  const isSavingOrUpdatingStatus = useAppSelector(
+    selectUpdateMarketListLoading
+  );
   const [products, setProducts] = useState<Product[]>(
     selectedMarketList?.products || []
   );
@@ -63,85 +68,99 @@ export const Shopping = () => {
 
   return (
     <SidebarComponent>
-      <MainContainer>
-        <MainFilterPage
-          secondaryBtnLabel="Finalizar Compra"
-          primaryBtnVariant="outline"
-          primaryBtnLabel="Salvar"
-          secondaryBtnVariant="destructive"
-          handleSecondaryBtn={handleFinishProductList}
-          handlePrimaryBtn={handleSaveProductList}
-          disablePrimaryBtn={
-            JSON.stringify(products) ===
-            JSON.stringify(selectedMarketList?.products)
-          }
-        />
-        <ScrollArea className="h-[calc(100vh-253px)] mt-6">
-          <Conditional condition={products.length > 0}>
-            <div className="max-w-[99%]">
-              <table
-                className="w-full border-separate"
-                style={{ borderSpacing: '0 16px' }}
-              >
-                <thead className="text-zinc-400">
-                  <tr>
-                    <th className="px-2 py-0 text-left font-semibold">
-                      Comprado
-                    </th>
-                    <th className="px-2 py-0 text-left font-semibold">
-                      Produto
-                    </th>
-                    <th className="px-2 py-0 text-left font-semibold">Qnt</th>
-                    <th className="px-2 py-0 text-left font-semibold">Valor</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {products.map((product, index) => (
-                    <tr
-                      key={index}
-                      className={'text-zinc-200'}
-                      style={{
-                        borderRadius: '0.5rem',
-                        boxShadow:
-                          '0 4px 6px 1px rgba(0,0,0,0.4), 0 1px 2px -2px rgba(0,0,0,0.1)',
-                        background: product.done ? '#27272a' : 'inherit'
-                      }}
-                    >
-                      <td className="px-4 py-5">
-                        <GenericCheckbox
-                          label={''}
-                          checked={product.done}
-                          onChange={(value) => handleProducts(index, value)}
-                        />
-                      </td>
-                      <td className="px-2 py-5">
-                        <span className={product.done ? 'line-through' : ''}>
-                          {product.name}
-                        </span>
-                      </td>
-                      <td className="px-2 py-5">
-                        <span className={product.done ? 'line-through' : ''}>
-                          {product.amount}
-                        </span>
-                      </td>
-                      <td className="px-2 py-5">
-                        <span className={product.done ? 'line-through' : ''}>
-                          {product.value}
-                        </span>
-                      </td>
+      <Conditional
+        condition={isSavingOrUpdatingStatus === ActionStatus.LOADING}
+      >
+        <LoadingSpinner />
+      </Conditional>
+      <Conditional
+        condition={isSavingOrUpdatingStatus !== ActionStatus.LOADING}
+      >
+        <MainContainer>
+          <MainFilterPage
+            secondaryBtnLabel="Finalizar Compra"
+            primaryBtnVariant="outline"
+            primaryBtnLabel="Salvar"
+            secondaryBtnVariant="destructive"
+            handleSecondaryBtn={handleFinishProductList}
+            handlePrimaryBtn={handleSaveProductList}
+            disablePrimaryBtn={
+              JSON.stringify(products) ===
+              JSON.stringify(selectedMarketList?.products)
+            }
+            disableSecondaryBtn={
+              isSavingOrUpdatingStatus === ActionStatus.LOADING
+            }
+          />
+          <ScrollArea className="h-[calc(100vh-253px)] mt-6">
+            <Conditional condition={products.length > 0}>
+              <div className="max-w-[99%]">
+                <table
+                  className="w-full border-separate"
+                  style={{ borderSpacing: '0 16px' }}
+                >
+                  <thead className="text-zinc-400">
+                    <tr>
+                      <th className="px-2 py-0 text-left font-semibold">
+                        Comprado
+                      </th>
+                      <th className="px-2 py-0 text-left font-semibold">
+                        Produto
+                      </th>
+                      <th className="px-2 py-0 text-left font-semibold">Qnt</th>
+                      <th className="px-2 py-0 text-left font-semibold">
+                        Valor
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </Conditional>
-          <Conditional condition={products.length === 0}>
-            <div className="flex flex-col items-center justify-center h-full">
-              <span className="text-zinc-200">Nenhum produto encontrado</span>
-            </div>
-          </Conditional>
-        </ScrollArea>
-      </MainContainer>
+                  </thead>
+                  <tbody>
+                    {products.map((product, index) => (
+                      <tr
+                        key={index}
+                        className={'text-zinc-200'}
+                        style={{
+                          borderRadius: '0.5rem',
+                          boxShadow:
+                            '0 4px 6px 1px rgba(0,0,0,0.4), 0 1px 2px -2px rgba(0,0,0,0.1)',
+                          background: product.done ? '#27272a' : 'inherit'
+                        }}
+                      >
+                        <td className="px-4 py-5">
+                          <GenericCheckbox
+                            label={''}
+                            checked={product.done}
+                            onChange={(value) => handleProducts(index, value)}
+                          />
+                        </td>
+                        <td className="px-2 py-5">
+                          <span className={product.done ? 'line-through' : ''}>
+                            {product.name}
+                          </span>
+                        </td>
+                        <td className="px-2 py-5">
+                          <span className={product.done ? 'line-through' : ''}>
+                            {product.amount}
+                          </span>
+                        </td>
+                        <td className="px-2 py-5">
+                          <span className={product.done ? 'line-through' : ''}>
+                            {product.value}
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </Conditional>
+            <Conditional condition={products.length === 0}>
+              <div className="flex flex-col items-center justify-center h-full">
+                <span className="text-zinc-200">Nenhum produto encontrado</span>
+              </div>
+            </Conditional>
+          </ScrollArea>
+        </MainContainer>
+      </Conditional>
     </SidebarComponent>
   );
 };
